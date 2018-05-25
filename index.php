835ad57d11bdc36d1155b0f302eb43c8e351dc59
AmNetDT <amnetdt@yahoo.com>
2018-05-25 07:42:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,9 @@ if(isset($_POST['submit'])){
    if($numRows==1){
 		
     		$_SESSION['client'] = $row['username'];
-			header("location: dashboard.php");
+				<script>
+			document.location="dashboard.php";
+			</script>
         
 		}else{
 			$fail = "Invalid Login Credentials";
@@ -45,10 +47,12 @@ if(isset($_POST['submit'])){
     
     
 			$_SESSION['client'] = $row['username'];
-			header("location: dashboard.php");
+				<script>
+			document.location="dashboard.php";
+			</script>
         
 		}else{
-			$fail = "Invalid Login Credentials";
+			$fail = "Invalid details";
 			
 		}
 	}
